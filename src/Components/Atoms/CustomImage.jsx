147b15f41635df884cc5b9b3a65c997dc0a5cd51
Riccardo Bresolin <124574800+RiccardoBres@users.noreply.github.com>
2024-03-25 14:29:57 +0000

    updated blur effect

--- src/Components/Atoms/CustomImage.jsx
+++ src/Components/Atoms/CustomImage.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
-const CustomImage = ({ src, alt, className, height, width }) => {
+const CustomImage = ({ src, alt, className }) => {
   return (
   <LazyLoadImage 
   src={src} 
   alt={alt} 
-  height={height}
-  width={width}
   className={`custom-image ${className}`} 
   loading='lazy'
   effect='blur'
+  placeholderSrc={src}
   />
   )};
 
