b852f3cc13ec4ee40e897b359a7867ab1ceb4884
Riccardo Bresolin <124574800+RiccardoBres@users.noreply.github.com>
2024-03-25 12:03:38 +0000

    updated loading image

--- src/Components/Atoms/CustomImage.jsx
+++ src/Components/Atoms/CustomImage.jsx
@@ -1,36 +1,18 @@
-import React, { useState } from 'react';
-import { motion } from 'framer-motion';
+import React from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
-const CustomImage = ({ src, alt, className }) => {
-  const [imageLoaded, setImageLoaded] = useState(false);
-
-  const handleImageLoad = () => {
-    setImageLoaded(true);
-  };
-
+const CustomImage = ({ src, alt, className, height, width }) => {
   return (
-    <div className={`custom-image d-flex align-items-center justify-content-center ${className}`}>
-      {!imageLoaded && (
-        <motion.div
-          initial={{ opacity: 0, filter: 'blur(20px)' }}
-          animate={{ opacity: 1, filter: 'blur(0px)' }}
-          transition={{ duration: 0.5 }}
-          style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}
-        />
-      )}
-      <motion.img
-        src={src}
-        alt={alt}
-        className={`custom-image ${className}`}
-        style={{ opacity: imageLoaded ? 1 : 0 }}
-        onLoad={handleImageLoad}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5 }}
-        loading="lazy"
-      />
-    </div>
-  );
-};
+  <LazyLoadImage 
+  src={src} 
+  alt={alt} 
+  height={height}
+  width={width}
+  className={`custom-image ${className}`} 
+  loading='lazy'
+  effect='blur'
+  />
+  )};
 
 export default CustomImage;
